f8ffc3932344b549a55576901dc2693dd228c215
Fabliha Hossain <fablihahossain@Fablihas-MBP.fios-router.home>
2019-12-30 00:27:44 -0500

    connection to database

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
 # Initializing Flask App
 from flask import Flask
 from flask import render_template
+from config import Config
+from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+db = MongoEngine()
+db.init_app(app)
+
+class Users(db.Document):
+	user_id = db.IntField(unique = True)
+	firstname = db.StringField(max_length = 50)
+	lastname = db.StringField(max_length = 50)
+	username = db.StringField(max_length = 50)
+	password = db.StringField(max_length = 50)
 
 # Defining Basic route
 @app.route("/")
 def index():
-	return render_template("index.html")
+	# Getting all users in db
+	users_list = Users.objects.all()
+	return render_template("index.html", users = users_list)
 
 # Running the flask app
 if __name__ == "__main__":
---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+class Config(object):
+	# Connecting to Database
+	MONGODB_SETTINGS = {'db' : 'photo_db'}
