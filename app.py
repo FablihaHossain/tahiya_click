6cb5085226e9f01d5e1e0a6dc13457964a314216
Fabliha Hossain <fablihahossain@Fablihas-MBP.fios-router.home>
2019-12-30 20:58:31 -0500

    moved some flask app code to __init__.py

--- app.py
+++ app.py
@@ -1,29 +1,21 @@
 # Initializing Flask App
-from flask import Flask
+# from flask import Flask
+# from flask_mongoengine import MongoEngine
+# from config import Config
+from application import app, db
 from flask import render_template
-from config import Config
-from flask_mongoengine import MongoEngine
+from models import Users, Albums
 
-app = Flask(__name__)
-app.config.from_object(Config)
+# app = Flask(__name__)
+# app.config.from_object(Config)
 
-db = MongoEngine()
-db.init_app(app)
-
-class Users(db.Document):
-	user_id = db.IntField(unique = True)
-	firstname = db.StringField(max_length = 50)
-	lastname = db.StringField(max_length = 50)
-	username = db.StringField(max_length = 50)
-	password = db.StringField(max_length = 50)
+# db = MongoEngine()
+# db.init_app(app)
 
 # Defining Basic route
 @app.route("/")
 def index():
 	# Getting all users in db
 	users_list = Users.objects.all()
-	return render_template("index.html", users = users_list)
 
-# Running the flask app
-if __name__ == "__main__":
-	app.run(debug = True)
+	return render_template("index.html", users = users_list)
