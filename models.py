de4daa844098e273758a9f804b7aefcd45956ea6
Fabliha Hossain <fablihahossain@Fablihas-MBP.fios-router.home>
2019-12-30 20:54:35 -0500

    creating models for database

---
+++ models.py
@@ -0,0 +1,15 @@
+import flask
+from application import db
+
+class Users(db.Document):
+	user_id = db.IntField(unique = True)
+	firstname = db.StringField(max_length = 50)
+	lastname = db.StringField(max_length = 50)
+	username = db.StringField(max_length = 50)
+	password = db.StringField(max_length = 50)
+
+class Albums(db.Document):
+	album_id = db.IntField(unique = True)
+	name = db.StringField(max_length = 50)
+	description = db.StringField(max_length = 100)
+	#images = db.ListField(ImageField())
