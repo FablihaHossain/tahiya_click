7a6eb2f310c8e6b1ea966d2468178cdce887fe5e
Fabliha Hossain <fablihahossain@Fablihas-MBP.fios-router.home>
2020-01-01 18:28:12 -0500

    modified models to include array of images in database

--- models.py
+++ models.py
@@ -1,5 +1,8 @@
+# Credit to https://tutorials.technology/tutorials/Mongoengine-How-to-use-filefield-for-storing-images-and-files.html
 import flask
 from application import db
+from mongoengine import Document
+from mongoengine import ListField, FileField, EmbeddedDocument, EmbeddedDocumentField
 
 class Users(db.Document):
 	user_id = db.IntField(unique = True)
@@ -8,8 +11,12 @@ class Users(db.Document):
 	username = db.StringField(max_length = 50)
 	password = db.StringField(max_length = 50)
 
+class AlbumImage(EmbeddedDocument):
+	image = FileField()
+
 class Albums(db.Document):
 	album_id = db.IntField(unique = True)
 	name = db.StringField(max_length = 50)
 	description = db.StringField(max_length = 100)
-	#images = db.ListField(ImageField())
+	images = ListField(EmbeddedDocumentField(AlbumImage))
+	
