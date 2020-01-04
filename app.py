2100b8be70337e1ff26ae58dbdefbbb1d9ad479d
Fabliha Hossain <fablihahossain@Fablihas-MBP.fios-router.home>
2020-01-04 18:36:01 -0500

    inserted initial data into albums table for testing

--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 # from config import Config
 from application import app, db
 from flask import render_template
-from models import Users, Albums, AlbumImage
+from models import Users, Albums 
 
 # app = Flask(__name__)
 # app.config.from_object(Config)
@@ -15,29 +15,17 @@ from models import Users, Albums, AlbumImage
 # Defining Basic route
 @app.route("/")
 def index():
+	#img = ["/static/images/IMG_4720.jpg", "/static/images/IMG_4721.jpg"]
+	#Albums(album_id = 1, name = "Nature Pictures", description = "Inspirational Pictures of Natural Environment", images = img).save()
+
 	# Getting all users in db
 	users_list = Users.objects.all()
 
-	# image1 = AlbumImage()
-	# imageFile1 = open('application/static/images/IMG_4721.jpg', 'rb')
-	# image1.image.put(imageFile1, content_type = 'image/png')
-
-	# image2 = AlbumImage()
-	# imageFile2 = open('application/static/images/IMG_4720.JPG', 'rb')
-	# image2.image.put(imageFile2, content_type = 'image/png')
-
-	# newAlbum = Albums()
-	# newAlbum.album_id = 1
-	# newAlbum.name = "Nature Photos"
-	# newAlbum.description = "Pictures of Nature"
-	# newAlbum.images.append(image1)
-	# newAlbum.images.append(image2)
-	# newAlbum.save()
-
+	# Getting all albums in db
 	albums_list = Albums.objects.all()
 	images = []
 	for album in albums_list:
-		for img in album.images:
-			images.append(img)
+		for imgs in album.images:
+			images.append(imgs)
 
 	return render_template("index.html", users = users_list, albums = albums_list, img = images)
