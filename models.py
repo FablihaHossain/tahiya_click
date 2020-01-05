9f5b5c8fee896b208f96544d2a368847f5988082
Fabliha Hossain <fablihahossain@Fablihas-MBP.fios-router.home>
2020-01-04 23:06:10 -0500

    updated album table to include album owner

--- models.py
+++ models.py
@@ -1,8 +1,7 @@
-# Credit to https://tutorials.technology/tutorials/Mongoengine-How-to-use-filefield-for-storing-images-and-files.html
 import flask
 from application import db
 from mongoengine import Document
-from mongoengine import ListField, FileField, EmbeddedDocument, EmbeddedDocumentField, StringField
+from mongoengine import ListField, StringField, IntField
 
 class Users(db.Document):
 	user_id = db.IntField(unique = True)
@@ -15,6 +14,10 @@ class Albums(db.Document):
 	album_id = db.IntField(unique = True)
 	name = db.StringField(max_length = 50)
 	description = db.StringField(max_length = 100)
+	# User ID of the one that created the album
+	owner_id = db.IntField()
 	# Array of names of Image Files
 	images = ListField(StringField(max_length = 200))
+
+
 	
