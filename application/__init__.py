dd3bbf3b2080661d5b89ceded31ee13bf4c9cc3b
Fabliha Hossain <fablihahossain@Fablihas-MBP.fios-router.home>
2020-01-12 17:46:20 -0500

    modified comments in app and db object creation

--- application/__init__.py
+++ application/__init__.py
@@ -3,8 +3,10 @@ from flask import Flask
 from flask_mongoengine import MongoEngine
 from config import Config
 
+# Creating the Flask App
 app = Flask(__name__)
 app.config.from_object(Config)
 
+# Database Connection
 db = MongoEngine()
 db.init_app(app)
