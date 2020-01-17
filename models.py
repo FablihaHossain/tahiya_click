cac65076ce38c281a4662eb68faaa71b6565addf
Fabliha Hossain <fablihahossain@Fablihas-MBP.fios-router.home>
2020-01-16 22:28:23 -0500

    small modification to models.py

--- models.py
+++ models.py
@@ -4,21 +4,21 @@ from mongoengine import Document
 from mongoengine import ListField, StringField, IntField
 
 # Users Table
-class Users(db.Document):
-	user_id = db.IntField(unique = True)
-	firstname = db.StringField(max_length = 50)
-	lastname = db.StringField(max_length = 50)
-	email = db.StringField(max_length = 100)
-	username = db.StringField(max_length = 50)
-	password = db.StringField(max_length = 100)
-	role = db.StringField(max_length = 20)
+class Users(Document):
+	user_id = IntField(unique = True)
+	firstname = StringField(max_length = 50)
+	lastname = StringField(max_length = 50)
+	email = StringField(max_length = 100)
+	username = StringField(max_length = 50)
+	password = StringField(max_length = 100)
+	role = StringField(max_length = 20)
 
 # Albums Table
-class Albums(db.Document):
-	album_id = db.IntField(unique = True)
-	name = db.StringField(max_length = 50)
-	description = db.StringField(max_length = 100)
-	owner_id = db.IntField() # User ID of the one that created the album
+class Albums(Document):
+	album_id = IntField(unique = True)
+	name = StringField(max_length = 50)
+	description = StringField(max_length = 100)
+	owner_id = IntField() # User ID of the one that created the album
 	images = ListField(StringField(max_length = 200)) # Array of names of Image Files
 
 
