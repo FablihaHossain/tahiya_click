09d03a80a091959e5bfe32e31138a3a1f369ad02
Fabliha Hossain <fablihahossain@Fablihas-MBP.fios-router.home>
2020-01-19 15:33:26 -0500

    removed commented out material

--- application/__init__.py
+++ application/__init__.py
@@ -2,17 +2,11 @@
 from flask import Flask
 from flask_mongoengine import MongoEngine
 from config import Config
-from flask_pymongo import PyMongo
-from pymongo import MongoClient
 
 # Creating the Flask App
 app = Flask(__name__)
 app.config.from_object(Config)
 
-#app.config[Config.MONGODB_URI]
-#app.config["MONGO_URI"] = "mongodb://localhost:27017/photo_db"
-
 # Database Connection
 db = MongoEngine()
-#db = PyMongo(app)
 db.init_app(app)
