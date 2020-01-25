2502f1eb01ec916e2d4db3bb19e02f03113f5032
Fabliha Hossain <fablihahossain@Fablihas-MBP.fios-router.home>
2020-01-24 22:55:08 -0500

    configured the flask app with the secret key

--- application/__init__.py
+++ application/__init__.py
@@ -5,8 +5,13 @@ from config import Config
 
 # Creating the Flask App
 app = Flask(__name__)
+
+# Configuring it with secret key
 app.config.from_object(Config)
+app.secret_key = Config.SECRET_KEY
 
 # Database Connection
 db = MongoEngine()
 db.init_app(app)
+
+# Credit to https://flask.palletsprojects.com/en/1.1.x/quickstart/
