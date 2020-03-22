81ef8375822af5fbbd61bcb6b9c3f6d9a221a45b
Fabliha Hossain <fablihahossain27@gmail.com>
2020-03-22 02:04:34 -0400

    updated the mongo db settings

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,7 @@
 # Initializing Flask App
 from flask import Flask
 from flask_mongoengine import MongoEngine
+from pymongo import MongoClient
 from config import Config
 
 # Creating the Flask App
@@ -10,9 +11,17 @@ app = Flask(__name__)
 app.config.from_object(Config)
 app.secret_key = Config.SECRET_KEY
 
-# Database Connection
+# client = MongoClient("mongodb://127.0.0.1:54758")
+# db = client.photo_db
+
+
+#Database Connection
 db = MongoEngine()
 db.init_app(app)
+app.config['MONGODB_SETTINGS'] = {'db': 'photo_db', 'username': Config.user, 'password': Config.pwd}
+
+# Initial connection for the application launch
+#db = db_connection()
 
 # File location for all the images
 UPLOAD_FOLDER = 'application/static/images/'
