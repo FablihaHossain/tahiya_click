a1e2bf8aa6d9201004156ce4cc55893f7f390e27
Fabliha Hossain <fablihahossain27@gmail.com>
2020-07-05 16:51:46 -0400

    initialized the flask app with the hashing extension

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,7 @@
 # Initializing Flask App
 from flask import Flask
 from flask_mongoengine import MongoEngine
+from flask_hashing import Hashing
 from pymongo import MongoClient
 from config import Config
 
@@ -11,17 +12,13 @@ app = Flask(__name__)
 app.config.from_object(Config)
 app.secret_key = Config.SECRET_KEY
 
-# client = MongoClient("mongodb://127.0.0.1:54758")
-# db = client.photo_db
-
+# Flask Hashing Extension
+hashing = Hashing(app)
 
 #Database Connection
 db = MongoEngine()
 db.init_app(app)
 app.config['MONGODB_SETTINGS'] = {'db': 'photo_db', 'username': Config.user, 'password': Config.pwd}
-
-# Initial connection for the application launch
-#db = db_connection()
 
 # File location for all the images
 UPLOAD_FOLDER = 'application/static/images/'
@@ -29,3 +26,4 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # Credit to https://flask.palletsprojects.com/en/1.1.x/quickstart/
 # Credit to https://flask.palletsprojects.com/en/1.1.x/patterns/fileuploads/
+# Credit to https://flask-hashing.readthedocs.io/en/latest/
